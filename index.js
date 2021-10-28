93a6de31e21daca01b36502edaf14f0a67a178d4
Peter <warrenp11@gmail.com>
2021-10-28 17:45:19 -0400

    changed prompt call

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
 const inquirer = require('inquirer');
 
-inquirer
-    .prompt([
-        {
-            type: 'list',
-            name: 'role',
-            message: 'What is this employee\'s role?',
-            choices: ['Engineer', 'Intern', 'Manager']
-        },
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is the employee\'s name?'
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: 'What is the employee\'s ID?'
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What is the employee\'s email?'
-        }
-    ])
-    .then((answers) => {
-        console.log(answers);
-    })
+const prompt = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is this employee\'s name?'
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is this employee\'s ID?'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is this employee\'s email?'
+            },
+            {
+                type: 'list',
+                name: 'role',
+                message: 'What is this employee\'s role?',
+                choices: ['Engineer', 'Intern', 'Manager']
+            }
+        ])
+        .then(({name, id, email, role}) => {
+            console.log({name, id, email, role});
+            // IF 'Engineer' is chosen...
+                // THEN ask for github username...
+            // IF 'Intern' is chosen...
+                // THEN ask for school...
+            // IF 'Manager' is chosen...
+                // THEN ask for office number...
+        });
+};
+
+prompt();
