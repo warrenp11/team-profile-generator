41615ca813fb4a502aa36b87872b4a21fc8866ef
Peter <warrenp11@gmail.com>
2021-10-28 00:59:49 -0400

    test pass, create employee object

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,1 +1,9 @@
 const Employee = require('../lib/Employee');
+
+test('creates an employee object', () => {
+    const employee = new Employee('tom', '11', 'tom@email.com');
+
+    expect(employee.name).toBe('tom');
+    expect(employee.id).toBe('11');
+    expect(employee.email).toBe('tom@email.com');
+});
