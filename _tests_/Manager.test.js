59e8654ac88cee20b69f2ab2d056bf0cbe126de0
Peter <warrenp11@gmail.com>
2021-10-28 13:26:49 -0400

    test pass, creates new Manager object

---
+++ _tests_/Manager.test.js
@@ -0,0 +1,10 @@
+const Manager = require('../lib/Manager');
+
+test('creates manager object', () => {
+    const manager = new Manager('dave', '1031', 'dave@email.com', '237');
+
+    expect(manager.name).toBe('dave');
+    expect(manager.id).toBe('1031');
+    expect(manager.email).toBe('dave@email.com');
+    expect(manager.office).toBe('237');
+});
