f32e42380075730c9bbe4e8d86c13ed38440c1b7
Peter <warrenp11@gmail.com>
2021-10-28 14:55:21 -0400

    test passed: Intern getSchool and getRole

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -8,3 +8,15 @@ test('creates an intern object', () => {
     expect(intern.email).toBe('intern@email.com');
     expect(intern.school).toBe('Rutgers');
 });
+
+test('get intern\'s school', () => {
+    const intern = new Intern('newbie', '00', 'intern@email.com', 'Rutgers');
+
+    expect(intern.getSchool()).toEqual(expect.stringContaining('Rutgers'));
+});
+
+test('get intern\'s role', () => {
+    const intern = new Intern('newbie', '00', 'intern@email.com', 'Rutgers');
+
+    expect(intern.getRole()).toEqual(expect.stringContaining('Intern'));
+});
