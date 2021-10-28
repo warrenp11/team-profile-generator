a85d68eea19fcb069dffa7f62bbfa39471848150
Peter <warrenp11@gmail.com>
2021-10-28 13:31:45 -0400

    test pass, manager getRole

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -8,3 +8,9 @@ test('creates manager object', () => {
     expect(manager.email).toBe('dave@email.com');
     expect(manager.office).toBe('237');
 });
+
+test('gets manager role', () => {
+    const manager = new Manager('dave', '1031', 'dave@email.com', '237');
+
+    expect(manager.getRole()).toEqual(expect.stringContaining('Manager'));
+});
